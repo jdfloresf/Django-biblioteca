b1dcc3728816834c3cef686c002cf6d003bcd7b9
Jonathan D. Flores Franco <41920271+jdfloresf@users.noreply.github.com>
2024-08-21 09:52:30 -0600

    Update urls.py

--- biblioteca/apps/lector/urls.py
+++ biblioteca/apps/lector/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(),
+    path('prestamo/', views.RegistrarPrestamo.as_view(), name='prestamo_registro'),
+    path('prestamo/', views.RegistrarPrestamo.as_view(), name='prestamo_registro'),
+    path('prestamo-mult/', views.AddMultiplePrestamo.as_view(), name='prestamo-mult_registro'),
 ]
