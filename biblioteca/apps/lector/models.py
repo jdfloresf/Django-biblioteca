4712fa899e7da7c684b771990fd9fe20f9ffcf2a
Jonathan D. Flores Franco <41920271+jdfloresf@users.noreply.github.com>
2024-08-21 09:50:52 -0600

    Update models.py

--- biblioteca/apps/lector/models.py
+++ biblioteca/apps/lector/models.py
@@ -1,36 +1,32 @@
 from django.db import models
+from django.db.models.signals import post_delete
+
 from apps.libro.models import Libro
+from apps.autor.models import Persona
+
+from .managers import PrestamoManager
 
 # Create your models here.
 
-class Lector(models.Model):
+class Lector(Persona):
     """Model definition for Lector."""
-
-    nombres = models.CharField(max_length=50)
-    apellidos = models.CharField(max_length=50)
-    nacionalidad = models.CharField(max_length=50)
-    edad = models.PositiveIntegerField(default=0)
-
     class Meta:
         """Meta definition for Lector."""
 
         verbose_name = 'Lector'
         verbose_name_plural = 'Lectores'
 
-    def __str__(self):
-        """Unicode representation of Lector."""
-        return self.nombres
-    
+
 class Prestamo(models.Model):
     """Model definition for Prestamo."""
 
     lector = models.ForeignKey(Lector, on_delete=models.CASCADE)
-    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE, related_name='libro_prestado')
     fecha_prestamo = models.DateField()
     fecha_devolucion = models.DateField(blank=True, null=True)
     devuelto = models.BooleanField()
 
-
+    objects = PrestamoManager()
 
     class Meta:
         """Meta definition for Prestamo."""
@@ -38,6 +34,17 @@ class Prestamo(models.Model):
         verbose_name = 'Prestamo'
         verbose_name_plural = 'Prestamos'
 
+    def save(self, *args, **kwargs):
+        self.libro.stok -= 1
+        self.save()
+        super(Prestamo, self).save(*args, **kwargs)
+
     def __str__(self):
         """Unicode representation of Prestamo."""
         return self.lector.nombres + '-' + str(self.libro)
+    
+def upadte_libro_stok(sender, insance, **kwargs):
+    insance.libro.stok += 1
+    insance.libro.save()
+
+post_delete.connect(upadte_libro_stok, sender=Prestamo)
